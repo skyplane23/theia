24bbcf0607b87fe5e7e9f430e85aac0e30898944
Vitaliy Gulyy <vgulyy@redhat.com>
2020-06-19 15:20:22 +0300

    Remove slash at the end of extension path
    Signed-off-by: Vitaliy Gulyy <vgulyy@redhat.com>

--- packages/plugin-ext/src/main/browser/webview/webview.ts
+++ packages/plugin-ext/src/main/browser/webview/webview.ts
@@ -365,7 +365,7 @@ export class WebviewWidget extends BaseWidget implements StatefulWidget {
     }
 
     protected preprocessHtml(value: string): string {
-        // replace <head><meta http-equiv="Content-Security-Policy" /></head>
+        // Comment <meta http-equiv="Content-Security-Policy" /> tag in HEAD section
         let html = value
             .replace(/<\s*meta([^>]+?(?=Content-Security-Policy)[^>]+)\/?>/gsm, (_, group) => {
                 return `<!-- meta ${group} -->`;
